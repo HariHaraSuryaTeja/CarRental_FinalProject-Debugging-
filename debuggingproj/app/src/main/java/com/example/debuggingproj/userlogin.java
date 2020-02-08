374d1215cdd37ba9b651cef31cb03ccecea3bee5
palanchi22 <palanchi22@gmail.com>
2020-02-08 16:18:06 -0500

    Register page and forgot password page created.

--- debuggingproj/app/src/main/java/com/example/debuggingproj/userlogin.java
+++ debuggingproj/app/src/main/java/com/example/debuggingproj/userlogin.java
@@ -2,7 +2,11 @@ package com.example.debuggingproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class userlogin extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class userlogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userlogin);
+        final Button register=(Button)findViewById(R.id.register);
+        TextView fgtpwd=(TextView)findViewById(R.id.fgtpwd);
+        fgtpwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(userlogin.this,userforgotpwd.class);
+                startActivity(i);
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(userlogin.this,registerpage.class);
+                startActivity(i);
+            }
+        });
     }
 }
