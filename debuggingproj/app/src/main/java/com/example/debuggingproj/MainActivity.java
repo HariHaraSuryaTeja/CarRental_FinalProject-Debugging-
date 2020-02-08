05ca9804d4dcf5c7c39475160a2d62e13ed997cf
Akhiljogiparthi <akhiljogiparthi96@gmail.com>
2020-02-08 15:16:20 -0500

    Admin loginpage and User loginpage created

--- debuggingproj/app/src/main/java/com/example/debuggingproj/MainActivity.java
+++ debuggingproj/app/src/main/java/com/example/debuggingproj/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, login.class);
+                Intent intent = new Intent(MainActivity.this, startscreen.class);
                 startActivity(intent);
                 finish();
 
